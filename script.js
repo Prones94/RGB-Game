e58f73dd6d7461dc4f6cce0589b96ce8a270234e
Ian Rones <ronesp.85@gmail.com>
2019-12-23 12:57:53 -0800

    [fix] reset button

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+let colors = generateRandomColors(6);
+let header = document.querySelector("h1")
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
+let resetButton = document.querySelector("#reset")
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,6 +20,8 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!"
             changeColors(clickedColor);
+            header.style.backgroundColor = clickedColor
+            resetButton.textContent = "Play Again?"
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again"
@@ -43,4 +40,38 @@ function changeColors(color) {
 function pickColor(){
      var randomColor = Math.floor(Math.random() * colors.length);
      return colors[randomColor];
-}
+};
+
+function generateRandomColors(num) {
+    // Make an array
+    var arr = [];
+    // Repeat num number of times
+    for (var i = 0; i < num; i++) {
+        // Add num random color and push to array
+        arr.push(randomColor())
+    }
+    // Return array
+    return arr;
+};
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256)
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+
+};
+
+resetButton.addEventListener("click", function(){
+    // Generate new random colors
+    colors = generateRandomColors(6)
+    // Pick random color from array
+    pickedColor = pickColor();
+    // Change color display to match picked Color
+    colorDisplay.textContent = pickedColor;
+    // Change color of array
+    for(i = 0;i < squares.length; i++){
+        squares[i].style.background = colors[i]
+    }
+    header.style.backgroundColor = "#232323"
+})
